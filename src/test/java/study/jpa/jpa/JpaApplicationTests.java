55493329115bfeb375a020003e34dc068b3e45e5
jaejoon <dkansk924@naver.com>
2021-06-14 17:43:32 +0900

    first commit

---
+++ src/test/java/study/jpa/jpa/JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package study.jpa.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
